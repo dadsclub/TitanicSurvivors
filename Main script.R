ee9c600f2eb620a0a569a757f19712fa6b65ed4b
Angel Sarmiento <angelsarmiento409@gmail.com>
2018-10-29 11:12:25 -0400

    Add files via upload
    Added A Logistic Regression model. 79% Accuracy. Lots of new visualisations and feature engineering done.

--- Main script.R
+++ Main script.R
@@ -1,23 +1,19 @@
 
 
 # Libraries ---------------------------------------------------------------
-library(ggplot2)
 library(tidyverse)
-library(mice)
 library(readr)
-library(scales)
-library(ggthemes)
 library(caret)
-library(classifierplots)
 
 # Importing data ----------------------------------------------------------
 testset <- read_csv("all/test.csv")
 trainingset <- read_csv("all/train.csv")
 
+validation <- read_csv("all/gender_submission.csv")
 ##Note that both sets are being binded so that feature engineering can be done to both
-fullset <- bind_rows(testset, trainingset)
+fullset <- bind_rows(trainingset, testset)
 
-
+set.seed(555)
 # Feature Engineering  ----------------------------------------------------
 
 ##grabbing the title from passenger names using Regexps
@@ -60,16 +56,15 @@ fullset$Child[fullset$Age >= 18] <- 'Adult'
 
 table(fullset$Child, fullset$Survived)
 
-##I am going to do the same with mothers in the data
-fullset$Mother <-  'Not Mother'
-fullset$Mother[fullset$Sex == "female" & fullset$Parch > 0 & fullset$Age > 18 & fullset$Title != "Miss"] <- 'Mother'
-
-
-table(fullset$Mother, fullset$Survived)
 
 ##setting them to 0s and 1s for later
 fullset$Mother <-  0
 fullset$Mother[fullset$Sex == "female" & fullset$Parch > 0 & fullset$Age > 18 & fullset$Title != "Miss"] <- 1
+
+##I am going to do the same with mothers in the data
+table(fullset$Mother, fullset$Survived)
+
+
 # Graphics/Visualisations -------------------------------------------------
 
 ##What is the relationship between embark location and survival?
@@ -89,14 +84,56 @@ ggplot(embark_fares, aes(x = Embarked, y = Fare, fill = factor(Survived))) +
   ggtitle("Survivor Embarkment/Fare boxplot")
   
 
+
 ##median travelling fare
 mean(trainingset$Fare)
 
 # Fun Statistics stuff ----------------------------------------------------
-trainingset <- fullset[892:1309,]
-testset <- testset[1:891,]
+testset <- fullset[892:1309,]
+trainingset <- fullset[1:891,]
+
 
 ##how about some correlations?
   ##Is there a correlation between being a mother and surviving?
+
+# Model Training/Testing --------------------------------------------------
+
+##for the first model fitting, I am going to use a Multiple Logistic Regression model
+##removed unneeded variables 
+glm.fit = glm(Survived ~ Pclass + Age + Sex, 
+              family = binomial,
+              data = trainingset)
+summary(glm.fit)
+
+##So we find that some variables are not significant to the data: Fare, Mother, Parch, and Child. So we will remove
+##them and retrain the model
+
+
+##finding the probabilities that people survived
+glm.probs = predict(glm.fit, type ="response")
+
+##Create vectors of class predictions based on whether the predicted probability of survival is 
+##greater than or less than 0.5
+glm.pred = rep("Died", 891)
+glm.pred[glm.probs>.5] = "Survived"
+
+##Generating a table showing what our model predicted versus the actual results of the TRAINING SET
+table(glm.pred, trainingset$Survived)
+
+
+
+##im going to use the caret package to build a random forest model
+ # <- train(Survived ~Pclass + Age + Sex, 
+ #                  data = trainingset, 
+ #                  method = "lm")
+
+
+# Test Data modeling ------------------------------------------------------
+
+## testing the model on the test set 
+glm.probs_test = predict(glm.fit, testset, type ="response")
+
+glm.pred_test = rep("Died", 418)
+glm.pred_test[glm.probs2>.5] = "Survived"
 
 
